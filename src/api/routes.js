4e14d5e31a555aee6d3bf7ad3a71798c404ff5c8
luizlaljr <luizlaljr@gmail.com>
2021-03-15 07:03:42 -0300

    Criadas rotas do controle de diagnostico

--- src/api/routes.js
+++ src/api/routes.js
@@ -3,6 +3,10 @@ const routes = express.Router();
 
 const DiagnosisController = require('./controllers/DiagnosisController')
 
-routes.get('/diagnosis',DiagnosisController.index);
+routes.get('/diagnosis', DiagnosisController.index);
+routes.post('/diagnosis', DiagnosisController.store);
+routes.get('/diagnosis/:diagnosis_id', DiagnosisController.show);
+routes.put('/diagnosis/:diagnosis_id', DiagnosisController.update);
+routes.delete('/diagnosis/:diagnosis_id', DiagnosisController.destroy);
 
 module.exports = routes;
