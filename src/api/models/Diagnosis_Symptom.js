4507d8f2b740c0c5ab4d35dedd7bb8192a0ca60c
luizlaljr <luizlaljr@gmail.com>
2021-03-15 06:03:29 -0300

    Criado modelo da classe diagnostico_sintoma

---
+++ src/api/models/Diagnosis_Symptom.js
@@ -0,0 +1,25 @@
+const {
+  Model,
+} = require('sequelize');
+
+class Diagnosis_Symptom extends Model{
+  static init(sequelize){
+      super.init({},{
+        sequelize,
+        tableName: 'diagnosis_symptom'
+    })
+  };
+
+  static associate(models) {
+      this.belongsTo(models.Diagnosis, {
+          foreignKey: 'diagnosis_id',
+          as: 'diagnostics',
+      });
+      this.belongsTo(models.Symptom, {
+          foreignKey: 'symptom_id',
+          as: 'symptoms',
+      });
+  }
+}
+
+module.exports = Diagnosis_Symptom;
