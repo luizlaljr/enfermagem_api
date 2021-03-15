9191e4228e243bc793108deccdc7d89aca048afb
luizlaljr <luizlaljr@gmail.com>
2021-03-15 15:36:20 -0300

    Add as rotas de sintomas, causas, busca e relacao

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,11 +2,32 @@ const express = require('express');
 const routes = express.Router();
 
 const DiagnosisController = require('./controllers/DiagnosisController')
+const SymptomController = require('./controllers/SymptomController')
+const Diagnosis_SymptomController = require('./controllers/Diagnosis_SymptomController');
+const CauseController = require('./controllers/CauseController')
+const Fech_DiagnosisController = require('./controllers/Fetch_DiagnosisController');
 
 routes.get('/diagnosis', DiagnosisController.index);
 routes.post('/diagnosis', DiagnosisController.store);
 routes.get('/diagnosis/:diagnosis_id', DiagnosisController.show);
 routes.put('/diagnosis/:diagnosis_id', DiagnosisController.update);
 routes.delete('/diagnosis/:diagnosis_id', DiagnosisController.destroy);
+
+routes.get('/symptom', SymptomController.index);
+routes.post('/symptom', SymptomController.store);
+routes.get('/symptom/:symptom_id', SymptomController.show);
+routes.put('/symptom/:symptom_id', SymptomController.update);
+routes.delete('/symptom/:symptom_id', SymptomController.destroy);
+
+routes.get('/diagnosis_symptom', Diagnosis_SymptomController.index);
+routes.post('/diagnosis_symptom', Diagnosis_SymptomController.store);
+
+routes.get('/cause', CauseController.index);
+routes.post('/cause', CauseController.store);
+routes.get('/cause/:cause_id', CauseController.show);
+routes.put('/cause/:cause_id', CauseController.update);
+routes.delete('/cause/:cause_id', CauseController.destroy);
+
+routes.get('/:symptom', Fech_DiagnosisController.index);
 
 module.exports = routes;
