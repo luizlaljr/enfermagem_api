5960c11de55b1808a1563a48aa4505f985b324af
luizlaljr <luizlaljr@gmail.com>
2021-03-15 06:00:54 -0300

    Adicionado o acesso as variaveis locais

--- src/api/server.js
+++ src/api/server.js
@@ -1,15 +1,17 @@
+require('dotenv').config({
+  path: '.local_env',
+});
+
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 
-require('./server');
+require('../api');
 
 const api = express();
 
 api.use(cors());
 api.use(express.json());
 api.use(routes);
-
-
 
 api.listen(process.env.PORT || 3333);
