79f9ea58d091c6f17942e08e154f075220470920
luizlaljr <luizlaljr@gmail.com>
2021-03-15 06:07:56 -0300

    Criado arquivo de migracao da tabela causa

---
+++ src/migrations/20210314122629-create-cause.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.createTable('causes', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      diagnosis_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'diagnostics',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.dropTable('causes');
+
+  }
+};
