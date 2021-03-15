708dafad3cdae5845da598c92717d12f416685e8
luizlaljr <luizlaljr@gmail.com>
2021-03-15 15:28:05 -0300

    Alterado o tipo do atributo nome para texto

--- src/migrations/20210314122629-create-cause.js
+++ src/migrations/20210314122629-create-cause.js
@@ -11,7 +11,7 @@ module.exports = {
         allowNull: false,
       },
       name: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         allowNull: false,
       },
       diagnosis_id: {
